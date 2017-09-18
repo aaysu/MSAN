42434208e9f48d74960f3ac3f5757c6b1128feac
Arda <aaysu@users.noreply.github.com>
2017-09-17 20:59:21 -0700

    Add files via upload

---
+++ RoomieRec/RoomieRec_app/webpage.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from json2html import *
+
+
+app = Flask(__name__)
+
+
+def searchpage():
+    return app.send_static_file('submit.html')
+
+
+def recpage(json):
+    return json2html.convert(json = json)
